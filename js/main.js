a168165e2884bbc80f3900f5810fff4f0199fadd
Mariana <mariana.ospinap@konradlorenz.edu.co>
2024-03-21 18:04:11 -0500

    FrontEnd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const chatContainer = document.getElementById('chat-container');
+
+        const chatContainer = document.getElementById('chat-container');
         const userInput = document.getElementById('user-input');
 
         userInput.addEventListener('keypress', function(e) {
@@ -12,17 +13,17 @@ const chatContainer = document.getElementById('chat-container');
             if (message.trim() === '') return;
 
             const userMessageElement = document.createElement('p');
-            userMessageElement.innerHTML = '<strong>User:</strong> ${message}';
+            userMessageElement.innerHTML = `<strong>User:</strong> ${message}`;
             chatContainer.appendChild(userMessageElement);
 
             // Aquí puedes agregar la lógica para que el SaludBot responda según el mensaje del usuario
             // Por ahora, simplemente enviaremos un mensaje de ejemplo de respuesta del SaludBot
             setTimeout(function() {
                 const doctorMessageElement = document.createElement('p');
-                doctorMessageElement.innerHTML = '<strong>SaludBot:</strong> = Entiendo. ¿Puedes proporcionarme más detalles sobre tu situación?';
+                doctorMessageElement.innerHTML = `<strong>SaludBot:</strong> Entiendo. ¿Puedes proporcionarme más detalles sobre tu situación?`;
                 chatContainer.appendChild(doctorMessageElement);
             }, 1000);
 
             userInput.value = '';
         }
-    
+ 
